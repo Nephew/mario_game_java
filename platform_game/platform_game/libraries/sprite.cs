309b402fe2841481974c5322790d660e09a2c461
Mathieu Rheaume <ddrmanxbxfr@gmail.com>
2011-10-30 23:26:47 -0400

    Updated sprite class.

--- platform_game/platform_game/libraries/sprite.cs
+++ platform_game/platform_game/libraries/sprite.cs
@@ -8,18 +8,71 @@ namespace platform_game.libraries
 {
     class sprite
     {
-        private Texture2D uneTextureEn2d;
+        private Texture2D _TextureEn2d;
+        private byte _nombreDeFrames;
+        private byte _frameActuelle;
 
-
-        public sprite(Texture2D uneTextureALoader)
+        /// <summary>
+        /// Initialise un sprite.
+        /// </summary>
+        /// <param name="nombreDeFrames">Minimum 1 sinon il y a pas de frames</param>
+        public sprite(Texture2D uneTextureALoader, byte nombreDeFrames)
         {
-            uneTextureEn2d = uneTextureALoader;
+            if (nombreDeFrames > 254)
+            {
+                throw new ArgumentException("Le nombre de frames est trop elevee");
+            }
+            _TextureEn2d = uneTextureALoader;
+            _frameActuelle = 1;
         }
 
-        public Texture2D texture
+        /// <summary>
+        /// Initialise un sprite.
+        /// </summary>
+        /// <param name="nombreDeFrames">Minimum 1 sinon il y a pas de frames</param>
+        /// <param name="frameEnDisplay">Le numero de la sprite qui est actuellement choisis</param>
+        public sprite(Texture2D uneTextureALoader, byte nombreDeFrames, byte frameEnDisplay)
+        {
+            if (nombreDeFrames > 254 || frameEnDisplay > 254)
+            {
+                throw new ArgumentException("Le nombre de frames est trop elevee");
+            }
+
+            _TextureEn2d = uneTextureALoader;
+            _frameActuelle = frameEnDisplay;
+        }
+
+        #region Accesseurs
+        public Texture2D TextureUsed
         {
             get
-            { return uneTextureEn2d; }
+            { return _TextureEn2d; }
+        }
+
+        public byte NombresDeFramesDansLeSprite
+        {
+            get
+            { return _nombreDeFrames; }
+        }
+
+        public byte FrameActuelle
+        {
+            get
+            { return _frameActuelle; }
+        }
+        #endregion
+
+        private void nextPartOfTheSprite()
+        {
+            if (_nombreDeFrames != 1)
+            {
+                if (_frameActuelle == _nombreDeFrames)
+                {
+                    _frameActuelle = 1;
+                }
+                else
+                    _frameActuelle++;
+            }
         }
     }
 }
